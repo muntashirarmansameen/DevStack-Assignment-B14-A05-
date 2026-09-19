d97923577254ffec78477c3ca0abc52951192934
muntashirarmansameen <140498202+muntashirarmansameen@users.noreply.github.com>
2026-09-19 11:54:21 +0600

    Update Vite config to include base path
    Add base path configuration for Vite.

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,5 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: './', // <-- এই লাইনটি যুক্ত বা আপডেট করুন
 })
